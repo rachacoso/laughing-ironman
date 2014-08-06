663dfaa0d4a93ababdcf014cabe0ac10a9a893f4
Roberto Achacoso <bertoa@gmail.com>
2014-08-06 16:12:39 -0700

    change New User behavior to not change current session user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,6 @@ class UsersController < ApplicationController
 			user.password = params[:user][:password]
 			user.password_confirmation = params[:user][:password_confirmation]
 			user.save!
-			session[:user_id] = user.id.to_s
 			redirect_to users_url
 
 		end
